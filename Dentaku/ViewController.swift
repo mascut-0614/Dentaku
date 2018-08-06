e2f6e799b50ed707ba4763498f53013025092f43
mascut-0614 <chocochan_i@dcl.cs.waseda.ac.jp>
2018-08-06 11:25:19 +0900

    First

---
+++ Dentaku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dentaku
+//
+//  Created by KOUYA IWASE on 2018/08/06.
+//  Copyright © 2018年 KOUYA IWASE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
